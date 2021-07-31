a6e3d436efde6cb2463e1dc8fbe03007b3db9c96
Belqeeskoshan <88009566+Belqeeskoshan@users.noreply.github.com>
2021-07-31 18:24:02 -0400

    Update Test.java

--- src/com/test/Test.java
+++ src/com/test/Test.java
@@ -4,11 +4,7 @@ public class Test {
 	
 	public void testMethod() {
 		
-<<<<<<< HEAD
-//		this is the new change.
-=======
-// 	this line is added recently.	
->>>>>>> dcb883de1fdfaaa158e309696e00d38ac094c287
+
 		
 		
 	}
